c5eaed269f561854972daf5c953bcefd6ed2e7aa
Keturah Howard <keturah.dev@gmail.com>
2020-04-03 14:02:32 -0700

    create functionality to list animals

--- AnimalShelterInterface/Controllers/AnimalsController.cs
+++ AnimalShelterInterface/Controllers/AnimalsController.cs
@@ -1,7 +1,20 @@
+using AnimalShelterInterface.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
 namespace AnimalShelterInterface.Controllers
 {
-  public class AnimalsController
+  public class AnimalsController : Controller
   {
-
+    public IActionResult Index()
+    {
+      var allAnimals = Animal.GetAnimals();
+      return View(allAnimals);
+    }
   }
 }
